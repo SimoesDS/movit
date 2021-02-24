d0f3e797c160d799f14571a38b58de62eb36f282
SimoesDS <simoes.sdgo@gmail.com>
2021-02-23 23:14:42 -0300

    adding left template with counter

--- src/componets/ExperienceBar.tsx
+++ src/componets/ExperienceBar.tsx
@@ -1,15 +1,17 @@
+import styles from '../styles/components/ExperienceBar.module.css';
+
 export interface ExperienceBarProps {
     experience: string;
 }
 
 export function ExperienceBar(props: ExperienceBarProps) {
     return (
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
                 <div style={{ width: `${ props.experience }%` }} />
 
-                <span className="current-experience" style={{ left: `${ props.experience }%` }}>
+                <span className={styles.currentExperience} style={{ left: `${ props.experience }%` }}>
                     { `${Math.round(600 * (Number(props.experience) / 100))}xp` }
                 </span>
             </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
+import styles from '../styles/pages/Home.module.css';
 import { ExperienceBar } from '../componets/ExperienceBar'
+import { Profile } from '../componets/Profile'
+import { CompletedChallenges } from '../componets/CompletedChallenges';
+import { Countdown } from '../componets/Countdown';
+
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={ styles.container }>
+      <Head>
+        <title>Iício | MoveIt</title>
+      </Head>
+      
       <ExperienceBar experience="57"/>
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
